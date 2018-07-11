f0850319b85f9bbd03c772cf16884c3fbe77a3d8
Nikunj Gupta <nikunj.nkgpta@gmail.com>
2018-07-11 23:50:13 +0530

    Adds Powerpc experiment for testing

---
+++ powerpc/README.md
@@ -0,0 +1,23 @@
+# This experiment is for Powerpc Platform
+
+To run the experiment, simply run the make file.
+
+```
+$ make
+```
+
+If build correctly, it should generate an executable `main`. Run the executable.
+
+```
+$ ./main
+```
+
+If the output turns out to be:
+
+```
+$ ./main
+initialize_main
+main
+```
+
+then things are working correctly on Powerpc. Else, I need to delve deeper to understand why it's failing.
---
+++ powerpc/wrap_libc.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+// This is the actual libc function that we will call from our wrapper function
+extern "C" int __real___libc_start_main (
+				int (*main)(int, char**, char**), int argc, char * * ubp_av,
+				void (*init) (void), void (*fini) (void),
+				void (*rtld_fini) (void), void (* stack_end));
+
+// Storing pointer to main
+int (*actual_main)(int, char**, char**) = nullptr;
+
+// Our custom entry point
+int initialize_main(int argc, char** argv, char** envp)
+{
+    std::cout << __FUNCTION__ << std::endl;
+
+    // Calling the C main function from our entry point
+    return actual_main(argc, argv, envp);
+}
+
+// Our wrapper for the libc function
+extern "C" int __wrap___libc_start_main (
+				int (*main)(int, char**, char**), int argc, char * * ubp_av,
+				void (*init) (void), void (*fini) (void),
+				void (*rtld_fini) (void), void (* stack_end))
+{
+
+    // Assigning the value of main to actual_main
+    actual_main = main;
+
+	// calling original __libc_start_main, but with our custom entry point implementation
+	return __real___libc_start_main(initialize_main, argc, ubp_av, init, fini, rtld_fini, stack_end);
+}
